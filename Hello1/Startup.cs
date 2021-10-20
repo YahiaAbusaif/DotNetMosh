2ebdaeffb3afda4907564266dbace42c30292835
Yahia A. Abusaif <yahiaali2014.ya@gmail.com>
2021-10-20 11:56:52 +0200

    try to Add project files

---
+++ Hello1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hello1.Startup))]
+namespace Hello1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
